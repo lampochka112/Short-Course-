4b29db6001613753a480eaf86311d716df747f9e
bobrdobr1z <bobrdobr112zxc@bk.ru>
2025-04-18 20:03:35 +0300

    sum of numbers

---
+++ task1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+    std::cout << "Сумма: " << num1 + num2 << std::endl;
+    return 0;
